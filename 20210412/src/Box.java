422d89aef692c8e8e50fba07a47a8034f026c1db
dltmdwlr <dltmdwlr@gmail.com>
2021-04-19 14:08:14 +0900

    스터디카페

---
+++ 20210412/src/Box.java
@@ -0,0 +1,22 @@
+class Apple{
+	public String toString() { return "»ç°ú"; }
+}
+class Orange{
+	public String toString() { return "¿À·»Áö"; }
+}
+class Box <T>{
+	Object o;
+	T obj;
+
+	public Box(T obj) {
+		this.obj = obj;
+	}
+
+	public T getObj() {
+		return obj;
+	}
+
+	public void setObj(T obj) {
+		this.obj = obj;
+	}
+}
---
+++ 20210412/src/DBox.java
@@ -0,0 +1,20 @@
+
+public class DBox<L,R> {
+	private L l;
+	private R r;
+	
+	// »ý¼ºÀÚ ctrl + shift + s -> o
+	public DBox(L l, R r) {
+		super();
+		this.l = l;
+		this.r = r;
+	}
+	
+	// toString ctrl + shift + s -> s
+	@Override
+	public String toString() {
+		return "DBox [l=" + l + ", r=" + r + "]";
+	}
+	
+	
+}
